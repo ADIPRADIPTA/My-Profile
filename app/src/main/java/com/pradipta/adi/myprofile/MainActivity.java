0fe574fe8eca6f9b0e3a88ad66765b545805e040
unknown <adipradipta93@gmail.com>
2018-02-25 15:30:11 +0700

    upload aplikasi

---
+++ app/src/main/java/com/pradipta/adi/myprofile/MainActivity.java
@@ -0,0 +1,80 @@
+package com.pradipta.adi.myprofile;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout llButtonInstagram, llButtonGithub, llButtonFacebook;
+    ImageButton imageButtoninstagram, imageButtongithub, imageButtonfacebook;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageButtoninstagram = (ImageButton) findViewById(R.id.imageButtoninstagram);
+        imageButtongithub = (ImageButton) findViewById(R.id.imageButtongithub);
+        imageButtonfacebook = (ImageButton) findViewById(R.id.imageButtonfacebook);
+
+        imageButtoninstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.instagram.com/pradipta6161/";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        imageButtongithub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.github.com/ADIPRADIPTA";
+
+                Intent g = new Intent(Intent.ACTION_VIEW);
+                g.setData(Uri.parse(url));
+                startActivity(g);
+            }
+        });
+
+        imageButtonfacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "http://www.facebook.com/arezpecta2?ref=bookmarks";
+
+                Intent f = new Intent(Intent.ACTION_VIEW);
+                f.setData(Uri.parse(url));
+                startActivity(f);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        this.close();
+    }
+    public void close() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Apakah Anda Benar-Benar ingin keluar?")
+                .setCancelable(false)
+                .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                }).show();
+    }
+}
